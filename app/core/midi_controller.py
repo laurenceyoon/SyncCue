97c6ded015ca8b6d69452778b9b654728b5c2a25
jiyun.park <laurenceyoon@gmail.com>
2023-09-25 18:56:47 +0900

    simplify midi controller codes

--- app/core/midi_controller.py
+++ app/core/midi_controller.py
@@ -4,19 +4,12 @@ import mido
 from mido import MetaMessage
 
 port_index = 0
-# port = mido.open_output()
 
 
 class MidiController:
     def __init__(self):
         self.outport = None
-        self.is_running = False
-        try:
-            outport = mido.open_output()
-        except OSError:
-            pass
-        else:
-            self.outport = outport
+        self.outport = mido.open_output()
 
     def play(self, midi_file_path):
         mid = mido.MidiFile(midi_file_path)
