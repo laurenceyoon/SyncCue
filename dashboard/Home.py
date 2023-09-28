18e485686245af42ccadc53b12affab2eb5b9bea
jiyun.park <laurenceyoon@gmail.com>
2023-09-28 16:45:28 +0900

    update dashboard Home

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -1,18 +1,51 @@
-import os
-import sys
-
 import streamlit as st
-from dotenv import load_dotenv
-
-load_dotenv()
-sys.path.insert(0, os.getenv("PYTHONPATH"))
+from pythonosc import udp_client
 
 
-def read_file(file):
-    with open(file, "r", encoding="utf-8") as f:
-        content = f.read()
-        return content
+def send_osc_msg(address, arguments):
+    osc_client.send_message(address, arguments)
+    st.text_area(
+        label="Log message",
+        value=f"Sent OSC Message.\nAddress: {OSC_SERVER_IP}:{OSC_SERVER_PORT}{address}, Arguments: {arguments}",
+        disabled=True,
+    )
+    st.toast("Done!", icon="✅")
 
 
-readme_content = read_file("README.md")
-st.markdown(readme_content, unsafe_allow_html=True)
+st.title("Normal Server (9999)")
+
+# Basic Info
+with st.container():
+    st.subheader("Basic OSC Configuration")
+    col1, col2 = st.columns(2)
+    with col1:
+        OSC_SERVER_IP = st.text_input("OSC Server IP", "127.0.0.1")
+    with col2:
+        OSC_SERVER_PORT = int(st.text_input("OSC Server Port", 9999))
+
+# OSC In
+with st.container():
+    st.subheader("OSC In")
+    osc_client = udp_client.SimpleUDPClient(OSC_SERVER_IP, OSC_SERVER_PORT)
+    st.caption("Choose an OSC message to send:")
+
+    # OSC Message List
+    with st.expander("/start"):
+        address = st.text_input("address", key="start_address", value="/start")
+        arguments = st.text_input("arguments", key="start_arguments", value="0") or None
+        if st.button("Send", key="start_send", use_container_width=True):
+            send_osc_msg(address, arguments)
+
+    with st.expander("/stop"):
+        address = st.text_input("address", key="stop_address", value="/stop")
+        arguments = st.text_input("arguments", key="stop_arguments", value="1") or None
+        if st.button("Send", key="stop_send", use_container_width=True):
+            send_osc_msg(address, arguments)
+
+    with st.expander("/playback"):
+        address = st.text_input("address", key="playback_address", value="/playback")
+        arguments = (
+            st.text_input("arguments", key="playback_arguments", value="1-2") or None
+        )
+        if st.button("Send", key="playback_send", use_container_width=True):
+            send_osc_msg(address, arguments)
--- dashboard2/Home.py
+++ dashboard2/Home.py
@@ -1,18 +1,46 @@
-import os
-import sys
-
 import streamlit as st
-from dotenv import load_dotenv
-
-load_dotenv()
-sys.path.insert(0, os.getenv("PYTHONPATH"))
+import pandas as pd
+from pythonosc import udp_client
 
 
-def read_file(file):
-    with open(file, "r", encoding="utf-8") as f:
-        content = f.read()
-        return content
+def send_osc_msg(address, arguments):
+    osc_client.send_message(address, arguments)
+    st.text_area(
+        label="Log message",
+        value=f"Sent OSC Message.\nAddress: {OSC_SERVER_IP}:{OSC_SERVER_PORT}{address}, Arguments: {arguments}",
+        disabled=True,
+    )
+    st.toast("Done!", icon="✅")
 
 
-readme_content = read_file("README.md")
-st.markdown(readme_content, unsafe_allow_html=True)
+# ====================================
+
+st.title("Candidate Server (8888)")
+
+# Basic Info
+with st.container():
+    st.subheader("Basic OSC Configuration")
+    col1, col2 = st.columns(2)
+    with col1:
+        OSC_SERVER_IP = st.text_input("OSC Server IP", "127.0.0.1")
+    with col2:
+        OSC_SERVER_PORT = int(st.text_input("OSC Server Port", 8888))
+
+df = pd.DataFrame(
+    {
+        "ID": ["0", "1-0", "1-1", "1-2", "2"],
+        "Title": ["반짝반짝 작은별", "시네마천국 파트 0", "시네마천국 파트 1", "시네마천국 파트 2", "왕벌의 비행"],
+    }
+)
+st.dataframe(df, column_config={"name": "ID"}, hide_index=True)
+
+# OSC In
+with st.container():
+    st.subheader("OSC In")
+    osc_client = udp_client.SimpleUDPClient(OSC_SERVER_IP, OSC_SERVER_PORT)
+
+    with st.expander("/playback"):
+        address = st.text_input("Token", key="playback_address", value="/playback")
+        arguments = st.text_input("ID", key="playback_arguments", value="1-2") or None
+        if st.button("Send", key="playback_send", use_container_width=True):
+            send_osc_msg(address, arguments)
