4537b178c68d8dd23a491e765c51b26288877d2d
jiyun.park <laurenceyoon@gmail.com>
2023-10-04 23:54:39 +0900

    add midi port connection timer

--- app/core/midi_controller.py
+++ app/core/midi_controller.py
@@ -1,12 +1,33 @@
 import mido
+import time
+import threading
 
-port_index = 0
+from app.config import CONNECTION_INTERVAL
 
 
 class MidiController:
-    def __init__(self):
-        self.outport = mido.open_output()
+    def __init__(self, connection_interval: int):
+        self.interval = connection_interval
+        self.outport = None
         self.is_running = False
+        self.connect_to_midi_port()
+
+    def connect_to_midi_port(self):
+        thread = threading.Thread(target=self._connect_to_midi_port)
+        thread.start()
+
+    def _connect_to_midi_port(self):
+        while True:
+            try:
+                if self.outport is None:
+                    self.outport = mido.open_output(autoreset=True)
+                    print(f"💡 MIDI PORT CONNECTED 🔌 {self.outport}")
+                    break
+            except Exception as e:
+                print(
+                    f"Failed to connect to MIDI port. Error: {e}. Retrying in {self.interval} seconds..."
+                )
+                time.sleep(self.interval)
 
     def play(self, midi_file_path):
         mid = mido.MidiFile(midi_file_path)
@@ -24,4 +45,4 @@ class MidiController:
         self.is_running = False
 
 
-midi_controller = MidiController()
+midi_controller = MidiController(connection_interval=CONNECTION_INTERVAL)
